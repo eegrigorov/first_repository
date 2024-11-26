5f6c6651c977401f93e1b2acca0b268b84307dd5
eegrigorov <grigorov.us@gmail.com>
2024-11-26 17:18:48 +0300

    added 3rd part of a course_2

--- part3/test_fixture.py
+++ part3/test_fixture.py
@@ -11,7 +11,6 @@ def browser():
     browser = webdriver.Chrome()
     return browser
 
-
 class TestMainPage1():
     # вызываем фикстуру в тесте, передав ее как параметр
     def test_guest_should_see_login_link(self, browser):
@@ -21,9 +20,6 @@ class TestMainPage1():
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
-
-
-
 
 @pytest.mark.xfail(strict=True)
 def test_succeed():
